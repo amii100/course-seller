5093e48118dbe21f09176f4f189e30154301b968
Amitesh Rakshit <amitesh@containova.com>
2024-10-14 21:32:09 +0530

    added node_modules and .env to gitignore

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ require('dotenv').config({ override: true });
 
 const port = process.env.PORT || 3000;
 
+
+
 // listen on port
 app.listen(port, () => {
     console.log(`Listening on port ${port}`)
