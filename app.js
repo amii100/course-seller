d87400200b21c06b4e1551053d51a06d3f0eb263
Amitesh Rakshit <amitesh@containova.com>
2024-10-14 21:43:35 +0530

    homepage route

--- app.js
+++ app.js
@@ -3,7 +3,14 @@ require('dotenv').config({ override: true });
 
 const port = process.env.PORT || 3000;
 
+// routes
 
+app.get('/', (req, res) => {
+    res.status(200).json({
+        status: "success",
+        message: "You are on the homepage."
+    })
+})
 
 // listen on port
 app.listen(port, () => {
