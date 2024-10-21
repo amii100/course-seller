a28ea7e69cb968d8bb083bcae48b8d9d68072fac
Amitesh Rakshit <amitesh@containova.com>
2024-10-21 16:37:25 +0530

    getting entries from req.query

--- controllers/courses.js
+++ controllers/courses.js
@@ -8,14 +8,22 @@ const getAllCourses = async (req, res) => {
 
 const postNewCourse = async (req, res) => {
     const client = await getClient();
-    const course_name = "New course";
-    const author = "New Author";
-    const price = 12000;
-    await client.query(`INSERT INTO course (course_name, author, price) values (${course_name}, ${author}, ${price})`);
+    const { course_name, author, price } = req.query;
+    await client.query(`INSERT INTO course (course_name, author, price) values ('${course_name}', '${author}', '${price}')`);
     res.status(201).json({success: true});
+}
+
+const deleteCourse = async (req, res) => {
+    // delete a course from the db list
+
+    const course_name = "New course";
+    const client = await getClient();
+    await client.query(`DELETE FROM course WHERE course_name = '${course_name}'`);
+    res.status(200).json({success: true});
 }
 
 module.exports = {
     getAllCourses,
-    postNewCourse
+    postNewCourse,
+    deleteCourse
 }
--- routes/courses.js
+++ routes/courses.js
@@ -1,8 +1,9 @@
 const Router = require('express').Router();
-const { getAllCourses, postNewCourse } = require('../controllers/courses');
+const { getAllCourses, postNewCourse, deleteCourse } = require('../controllers/courses');
 
 Router.get('/', getAllCourses);
 Router.post('/', postNewCourse);
+Router.delete('/', deleteCourse);
 
 
 module.exports = Router;
